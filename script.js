a2b72bc22db9abb8cdec3115eb9fdf2820e7b89d
Poffers <mrfrozenfirefly@gmail.com>
2022-08-04 11:49:58 -0700

    Add slider functionality to change grid size

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
 let mouseDown = false;
 let erasing = false;
 
-function resetSquares() {
-    const canvas = document.querySelector(".canvas");
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.lastChild);
-    }
+function resetSquares(size) {
+    document.body.removeChild(document.getElementById("canvas"));
 
-    for (let i = 1; i <= 400; i++) {
-        let div = document.createElement("div");
+    let canvas = document.createElement("div");
+    canvas.id = "canvas";
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 1; i <= size * size; i++) {
+        const div = document.createElement("div");
         div.classList.add("cell");
+        div.style.color = "#ffffff";
         canvas.appendChild(div);
     }
+
+    document.body.insertBefore(canvas, document.getElementById("settings"));
+}
+
+function createSliderListeners() {
+    document.getElementById("size-slider").addEventListener("change", e => {
+        resetSquares(parseInt(e.target.value));
+        createDrawListeners();
+    });
+    document.getElementById("size-slider").addEventListener("input", e => {
+        document.getElementById("size-label").textContent = `${e.target.value} x ${e.target.value}`;
+    });
+}
+
+function createButtonListeners() {
+    document.getElementById("button-clear").addEventListener("click", () => {
+        resetSquares(parseInt(document.getElementById("size-slider").value));
+        createDrawListeners();
+    });
+    
+    document.getElementById("button-brush").addEventListener("click", () => {
+        erasing = false;
+        document.getElementById("button-brush").classList.add("active");
+        document.getElementById("button-eraser").classList.remove("active");
+    });
+    
+    document.getElementById("button-eraser").addEventListener("click", () => {
+        erasing = true;
+        document.getElementById("button-eraser").classList.add("active");
+        document.getElementById("button-brush").classList.remove("active");
+    });
 }
 
 function createDrawListeners() {
     document.querySelectorAll(".cell").forEach(box => box.addEventListener("mousedown", e => {
         colorBox(e.target);
     }));
-
     document.querySelectorAll(".cell").forEach(box => box.addEventListener("mouseover", e => {
         if (mouseDown) {
             colorBox(e.target);
         }
     }));
+    document.querySelector(".cell").addEventListener("touchstart", e => colorBox(e.target));
+    document.getElementById("canvas").addEventListener("touchmove", e => {
+        const touchedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+        if (touchedElement && touchedElement.className === "cell") {
+            colorBox(touchedElement);
+        }
+    });
 
 }
 
@@ -31,37 +71,15 @@ function colorBox(div) {
     if (!erasing) {
         div.style.backgroundColor = document.getElementById("color-picker").value;
     } else {
-        div.style.backgroundColor = "#ffffff"
+        div.style.backgroundColor = "#ffffff";
     }
 }
 
 document.body.addEventListener("mousedown", () => mouseDown = true);
 document.body.addEventListener("mouseup", () => mouseDown = false);
-document.querySelector(".canvas").addEventListener("touchmove", e => {
-    const touchedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
-    if (touchedElement && touchedElement.className === "cell") {
-        colorBox(touchedElement);
-    }
-});
-document.querySelector(".canvas").addEventListener("touchstart", e => colorBox(e.target));
 
-resetSquares();
+resetSquares(16);
 createDrawListeners();
-
-document.getElementById("button-clear").addEventListener("click", () => {
-    resetSquares();
-    createDrawListeners();
-});
-
-document.getElementById("button-brush").addEventListener("click", () => {
-    erasing = false;
-    document.getElementById("button-brush").classList.add("active");
-    document.getElementById("button-eraser").classList.remove("active");
-});
-
-document.getElementById("button-eraser").addEventListener("click", () => {
-    erasing = true;
-    document.getElementById("button-eraser").classList.add("active");
-    document.getElementById("button-brush").classList.remove("active");
-});
+createButtonListeners();
+createSliderListeners();
 
